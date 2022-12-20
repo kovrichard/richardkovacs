842e4f2595594988d97b39eb6b4d6dfc140c5a66
Richard Kovacs <richardkovacs.it@gmail.com>
2022-12-20 13:28:13 +0100

    feat: add google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
+import Head from 'next/head';
+import Script from 'next/script';
+
 export default function App() {
+	const isProd = process.env.NODE_ENV === "production";
+  console.log(isProd)
+
 	return (
-		<h1>Hello</h1>
+		<>
+			<Head>
+				{ isProd && (
+					<>
+						<Script
+							src="https://www.googletagmanager.com/gtag/js?id=G-GX10NYC1H6"
+							strategy="afterInteractive"
+						/>
+						<Script>
+							{`
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+
+								gtag('config', 'G-GX10NYC1H6');
+							`}
+						</Script>
+					</>
+				)}
+			</Head>
+			<h1>Hello</h1>
+		</>
 	)
 }
